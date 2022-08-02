ea4596c11b7c4ac46ea7f221412dcaedfcce04a9
Diego Llaya <goya0310@gmail.com>
2022-08-02 19:19:33 -0300

    primer tarea

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let sum = 0;
 
+    for (let i = 0; i < num.length; i++) {
+       sum += +num[i] * 2 ** (num.length - 1 - i);
+    }
+    return sum;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var Binario = [];
 
+  while (num !== 0){
+    Binario.unshift(num % 2);
+    num = Math.floor(num/2);
+  } 
+  
+  return Binario.join('');
 }
 
 
